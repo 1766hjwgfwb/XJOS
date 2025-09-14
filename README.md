0f057d7fafee6b706b9dd1ed7e341fbd31280f1c
1766hjwgfwb <hjwgfwb@gmail.com>
2025-09-14 11:28:29 +0800

    read disk

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Hos
+# XJOS
 
-- 用于教学研究的开源OS项目
+- 用于教学研究的OS项目
 
 
 - 开发环境
   - WSL2 + vscode remote + Ubuntu 24.04
   - gcc >= 13.3.0
+  - gdb >= 15.0.50.20240403-git
   - make >= 4.3
   - NASM >= 2.16.01
   - QEMU >= 8.2.2
---
+++ src/boot/boot.asm
@@ -0,0 +1,131 @@
+[org 0x7c00]
+
+
+mov ax, 3
+int 0x10 ; al = 0x00, ah = 0x03
+
+; init 
+mov ax, 0
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7c00
+
+; xchg bx, bx ; bochs debug
+
+mov si, string
+call print
+
+xchg bx, bx ; bochs debug
+
+mov edi, 0x1000     ; cpu -> memory 0x1000
+mov ecx, 0          ; staring sector
+mov bl, 1           ; sector size
+
+
+
+call read_disk
+
+xchg bx, bx ; bochs debug
+
+
+
+jmp $
+
+
+print:
+    mov ah, 0x0e    ; mdoe
+.next:
+    mov al, [ds:si]    ; mov al [ds:si]
+    cmp al, 0
+    jz .done
+    int 0x10        ; load ax
+    inc si
+    jmp .next
+
+.done:
+    ret
+
+
+read_disk:
+    mov dx, 0x1f2     ; read sector acomount
+    mov al, bl
+    out dx, al        ; al/ax/eax
+
+    ; 0 ~ 7 bits
+    inc dx ; dx + 1 = 0x1f3
+    mov al, cl
+    out dx, al
+
+    ; 8 ~ 15 bits
+    inc dx
+    shr ecx, 8
+    mov al, cl
+    out dx, al
+
+    ; 16 ~ 23 bits
+    inc dx
+    shr ecx, 8
+    mov al, cl
+    out dx, al
+
+    ; 24 ~ 27 bits
+    inc dx
+    shr ecx, 8  ; now 24 ~ 31 bits
+    and cl, 0b1111  ; set 24 ~ 27
+    mov al, 0b1110_0000 ; set 28 ~ 31
+    or al, cl
+    out dx, al  ; LBA mode
+
+    inc dx
+    mov al, 0x20    ; read command
+    out dx, al
+
+
+    xor ecx, ecx     ; clear ecx
+    mov cl, bl       ; sector size
+
+    .read:
+        push cx
+        call .waits     ; wait for disk ready
+        call .reads     ; read sector
+        pop cx
+        loop .read      ; `cx` control cycling amount
+    
+    ret
+
+    .waits:
+        mov dx, 0x1f7    ; disk status
+        .check
+            in al, dx
+            ; delay
+            jmp $+2
+            jmp $+2
+            jmp $+2
+        
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000 ; disk ready
+            jnz .check
+        ret
+
+    .reads:
+        mov dx, 0x1f0    ; data port
+        mov cx, 256      ; read 256 words (512 bytes)
+        .readw:
+            in ax, dx
+            jmp $+2
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2    ; next word
+            loop .readw
+        ret
+
+
+
+string:
+    db "Booting XJOS...", 10, 13, 0  ; '\n' '\r'
+
+
+times 510 - ($ - $$) db 0
+db 0x55, 0xaa
