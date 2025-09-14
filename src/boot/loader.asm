9da8052567eadc22f783ada11bae4028906e9671
1766hjwgfwb <hjwgfwb@gmail.com>
2025-09-14 23:38:01 +0800

    Loading Kernel

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -41,22 +41,7 @@ detect_memory:
     mov si, detecting
     call print
 
-    xchg bx, bx ; bochs debug
-
-    mov cx, [ards_count]     ; get count of entries
-    mov si, 0
-
-.show:
-    mov eax, [ards_buffer + si] ; get entry
-    mov ebx, [ards_buffer + si + 8]
-    mov edx, [ards_buffer + si + 16]
-    ; why dont use ecx?
-    ; because ecx is used to count entries(cx)
-    add si, 20
-    xchg bx, bx
-    loop .show
-
-jmp $
+    jmp prepare_protected_mode  ; jump to protected mode
 
 
 print:
@@ -84,6 +69,93 @@ error:
     call print
     hlt
     .msg db  "Loading failed.", 10, 13, 0
+
+
+prepare_protected_mode:
+    
+    cli; off interrupt
+
+    ; turn on 0x92
+    in al, 0x92
+    or al, 0b10
+    out 0x92, al
+
+    lgdt [gdt_ptr]   ; load gdt
+
+    ; startup protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; dword 4 bytes (32bits)
+    jmp dword code_selector:protected_mode
+    ; code_selector: 0x08
+
+[bits 32]
+protected_mode:
+    mov ax, data_selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esp, 0x10000  ; stack pointer
+
+    mov byte [0xb8000], 'P'
+
+    mov byte [0x200000], 'H'    ; test 0 ~ 4GB memory
+
+    xchg bx, bx ; bochs debug
+
+jmp $
+
+
+code_selector equ (1 << 3) ; 0x08
+data_selector equ (2 << 3) ; 0x10
+
+memory_base equ 0   ; memory base address
+memory_limit equ ((1024 * 1024 * 1024 * 4) / (1024 * 4)) - 1 ; memory limit address (20bits)
+
+
+; bochs gdtr 0x100F0(0x17) gdt_base((gdt_end - gdt_base) - 1)
+gdt_ptr:
+    dw (gdt_end - gdt_base) - 1   ; limit 0 ~ 15bits
+    dd gdt_base   ; int base
+
+gdt_base:
+    dd 0, 0         ; null code selector 4 bytes, null data selector 4 bytes
+gdt_code:
+    dw memory_limit & 0xffff   ; segmet limit low 0 ~ 15bits
+    dw memory_base & 0xffff    ; base address low 0 ~ 15bits
+    db (memory_base >> 16) & 0xff   ; base address high 16 ~ 23bits
+    
+    ; exist - DLP - S - code - C - R - A 
+    ; P | DPL(2bits) | S | TYPE | C/E | R/W | A |
+    db 0b_1_00_1_1_0_1_0 
+
+    ; 4k - 32bits - no_64 - available - limit 16 ~ 19bits
+    db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf
+    
+    ; base address high 24 ~ 31bits
+    db (memory_base >> 24) & 0xff
+
+gdt_data:
+    dw memory_limit & 0xffff   ; segmet limit low 0 ~ 15bits
+    dw memory_base & 0xffff    ; base address low 0 ~ 15bits
+    db (memory_base >> 16) & 0xff   ; base address high 16 ~ 23bits
+    
+    ; exist - DLP - S - data - E(up) - W - A 
+    ; P | DPL(2bits) | S | TYPE | C/E | R/W | A |
+    db 0b_1_00_1_0_0_1_0 
+
+    ; 4k - 32bits - no_64 - available - limit 16 ~ 19bits
+    db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf
+    
+    ; base address high 24 ~ 31bits
+    db (memory_base >> 24) & 0xff
+
+gdt_end:
 
 ards_count:
     dw 0
